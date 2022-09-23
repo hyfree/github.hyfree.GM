2a866c13522344934d2502f7a653cbc0eca21f2d
hyfree <wangxianqiangcn@foxmail.com>
2022-09-23 17:48:32 +0800

    提交

--- github.hyfree.GMTests/GMServiceTests.cs
+++ github.hyfree.GMTests/GMServiceTests.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+using System.Diagnostics;
+
 namespace github.hyfree.GM.Tests
 {
     [TestClass()]
@@ -12,6 +14,25 @@ namespace github.hyfree.GM.Tests
             var pubk = "04F54CEEB470BAFCCE989A98D65BE1AEF562FC0C94DE152A1D658689E1D01692E7BB81C76DBA09CEF76C1386F9E0D02846F3C28BBDB11D697E9DE56341F90B1DE3";
             GMService gMService = new GMService();
             var enc = gMService.SM2Encrypt(data, pubk);
+            Console.WriteLine(enc);
+        }
+
+        public void SM2EncryptPerformanceTest()
+        {
+            byte[] buffer=new byte[4096];
+            var pubkBuffer = HexUtil.HexToByteArray("04F54CEEB470BAFCCE989A98D65BE1AEF562FC0C94DE152A1D658689E1D01692E7BB81C76DBA09CEF76C1386F9E0D02846F3C28BBDB11D697E9DE56341F90B1DE3");
+            for (int i = 0; i < 4096; i++)
+            {
+                buffer[i] = (byte)(i % 256);
+            }
+
+            Stopwatch stopwatch=new Stopwatch();
+            stopwatch.Start();
+           
+          
+            GMService gMService = new GMService();
+            var enc = gMService.SM2Encrypt(buffer, pubk);
+            stopwatch.Stop();
             Console.WriteLine(enc);
         }
 
